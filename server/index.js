13b817c63638fe49d04bc9b88605207f6d90ace3
Bhavyasai Survepalli <bhavyasaisurvepalli@Bhavyasais-MacBook-Pro.local>
2022-05-04 18:20:32 -0400

    add: chage port

--- server/index.js
+++ server/index.js
@@ -26,6 +26,6 @@ app.get("/customers/:customer_id", async(req,res)=>{
     }
 })
 
-app.listen(5001, () => {
-    console.log("server has started on port 5001");
+app.listen(5000, () => {
+    console.log("server has started on port 5000");
 });
